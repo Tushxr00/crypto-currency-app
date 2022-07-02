498305d611a62437f97a0e387a6473947c760b50
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-02 16:01:09 +0530

    created a basic navbar layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import { Layout, Space, Typography } from "antd";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+import icon from "../images/633px-Cryptocurrency_Logo.svg.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/"> Crypto Hub</Link>
+        </Typography.Title>
+        {/* <Button ClassName="menu-control-container"></Button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
