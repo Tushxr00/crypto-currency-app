7e6efb09e43537fdceeee3476b4b568af032c399
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-02 16:13:07 +0530

    alternative way to import componts to reduce code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import { Layout, Space, Typography } from "antd";
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
