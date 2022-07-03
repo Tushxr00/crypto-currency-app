9cf97025f100d22220f74894f42b20fb39790cec
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-03 19:50:21 +0530

    added a news filter

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,28 +2,50 @@ import React, { useEffect, useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const News = (props) => {
-  const count = props.simplified ? 10 : 44;
-  const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
-    count: count,
-  });
-  const newsData = cryptoNews?.value;
-  const [newList, setNewList] = useState([]);
-
   const demoImage =
     "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+  const count = props.simplified ? 10 : 44;
+  const [newList, setNewList] = useState([]);
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count,
+  });
+  const { data: cryptosList } = useGetCryptosQuery(100);
+
+  const newsData = cryptoNews?.value;
+  const coinList = cryptosList?.data?.coins;
 
   useEffect(() => {
     setNewList(newsData);
   }, [newsData]);
-  console.log({ cryptoNews });
-  console.log({ newsData });
 
   if (!newsData) return "Loading ...";
+
   return (
     <Row gutter={[24, 24]}>
+      {!props.simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a news crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Select.Option value="Cryptocurrency">Cryptocurrency</Select.Option>
+            {coinList.map((coin) => (
+              <Select.Option value={coin.name}>{coin.name}</Select.Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {newList?.map((news, index) => (
         <Col xs={24} sm={12} lg={8} key={index}>
           <Card hoverable className="news-card">
