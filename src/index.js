ad3d9ff53054703f361d51e1a1bff1ce8cd8b113
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-03 11:43:20 +0530

    created store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.css"
+import "antd/dist/antd.css";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
