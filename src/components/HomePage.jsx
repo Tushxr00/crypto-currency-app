1b6eb055ba54e152858ad4355475f134fc890084
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-03 11:11:55 +0530

    added some data to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,33 @@
+import millify from "millify";
 import React from "react";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Typography.Title level={2} className="heading">
+        Global Crypto Stats
+      </Typography.Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market" value={5} />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default HomePage;
