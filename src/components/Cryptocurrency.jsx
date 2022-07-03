86b9f6db4500cbd08844a196368bc4b4e836dbf0
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-03 16:11:33 +0530

    removed searh from homepage

--- src/components/Cryptocurrency.jsx
+++ src/components/Cryptocurrency.jsx
@@ -7,18 +7,34 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrency = (props) => {
   const count = props.simplified ? 10 : 100;
   const { data: cryptosList } = useGetCryptosQuery(count);
-  const data = cryptosList?.data?.coins;
+  const coinList = cryptosList?.data?.coins;
   const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // useEffect(() => {
+  //   console.log(coinList);
+  //   setCryptos(coinList);
+  // }, [coinList]);
 
   useEffect(() => {
-    console.log(data);
-    setCryptos(data);
-  }, [data]);
+    const filteredData = coinList.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm)
+    );
+    setCryptos(filteredData);
+  }, [coinList, searchTerm]);
 
   if (!cryptos) return "Loading ...";
 
   return (
     <React.Fragment>
+      {!props.simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Crypto currency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="cryptos-card-container">
         {cryptos.map((currency) => (
           <Col
