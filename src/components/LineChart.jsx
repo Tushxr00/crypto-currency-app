b8c8698aed5e364b9df30ef60530ca29632a051c
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-05 19:12:53 +0530

    added the line chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,8 +1,66 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+// import {
+//   Chart as ChartJS,
+//   LineElement,
+//   PointElement,
+//   LinearScale,
+//   Title,
+//   CategoryScale,
+// } from "chart.js";
+import Chart from "chart.js/auto";
 import { Typography, Row, Col } from "antd";
 
+// ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale);
+
 const LineChart = (props) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  console.log(props);
+
+  for (let i = props.coinHistory?.data?.history?.length - 1; i > 0; i--) {
+    coinPrice.push(props.coinHistory.data.history[i].price);
+    coinTimestamp.push(
+      new Date(
+        Number(`${props.coinHistory.data.history[i].timestamp}000`)
+      ).toLocaleDateString()
+    );
+  }
+
+  console.log({ coinPrice, coinTimestamp });
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    scale: {
+      ticks: {
+        beginAtZero: true,
+      },
+    },
+    legend: {
+      display: true,
+    },
+  };
+
+  if (!props.coinHistory) return "loading...";
+
   return (
     <React.Fragment>
       <Row className="chart-header">
@@ -11,10 +69,14 @@ const LineChart = (props) => {
         </Typography.Title>
         <Col className="price-container">
           <Typography.Title level={5} className="price-change">
-            {props.coinHistory?.data?.change}
+            {props.coinHistory?.data?.change} %
+          </Typography.Title>
+          <Typography.Title level={5} className="current-price">
+            Current {props.coinName} Price: ${props.currentPrice}
           </Typography.Title>
         </Col>
       </Row>
+      <Line data={data} options={options} />
     </React.Fragment>
   );
 };
