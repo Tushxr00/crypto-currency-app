a21e0e6f1c4432332e68053ebd96100387eb0aae
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-05 19:26:53 +0530

    added a menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {  Menu, Typography, Avatar } from "antd";
+import React, { useEffect, useState } from "react";
+import { Menu, Typography, Avatar, Button } from "antd";
 import { Link } from "react-router-dom";
 import {
   HomeOutlined,
   MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
+  MenuOutlined,
 } from "@ant-design/icons";
 
 import icon from "../images/cryptocurrency.png";
@@ -34,6 +35,27 @@ const menuItems = [
 ];
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -41,8 +63,14 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto Hub</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu((prevState) => !prevState)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark" items={menuItems} />
+      {activeMenu && <Menu theme="dark" items={menuItems} />}
     </div>
   );
 };
